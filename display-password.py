be72bcb6c9b66664e62063550c8cee4dd9998daa
Andrew Erickson <aerickson@gmail.com>
2024-01-15 23:55:35 -0500

    run black and ruff

--- display-password.py
+++ display-password.py
@@ -1,4 +1,4 @@
-# display-password shows recently cracked passwords on the pwnagotchi display 
+# display-password shows recently cracked passwords on the pwnagotchi display
 #
 #
 ###############################################################
@@ -11,23 +11,21 @@ from pwnagotchi.ui.components import LabeledValue
 from pwnagotchi.ui.view import BLACK
 import pwnagotchi.ui.fonts as fonts
 import pwnagotchi.plugins as plugins
-import pwnagotchi
 import logging
 import os
 
 
 class DisplayPassword(plugins.Plugin):
-    __author__ = '@nagy_craig'
-    __version__ = '1.0.1'
-    __license__ = 'GPL3'
-    __description__ = 'A plugin to display recently cracked passwords'
-        
+    __author__ = "@nagy_craig"
+    __version__ = "1.0.1"
+    __license__ = "GPL3"
+    __description__ = "A plugin to display recently cracked passwords"
+
     def __init__(self):
         self.peers_detected = False
-    
+
     def on_loaded(self):
         logging.info("display-password loaded")
-
 
     def on_ui_setup(self, ui):
         if ui.is_waveshare_v2():
@@ -52,30 +50,43 @@ class DisplayPassword(plugins.Plugin):
             h_pos = (0, 91)
             v_pos = (180, 61)
 
-        if self.options['orientation'] == "vertical":
-            ui.add_element('display-password', LabeledValue(color=BLACK, label='', value='',
-                                                   position=v_pos,
-                                                   label_font=fonts.Bold, text_font=fonts.Small))
+        if self.options["orientation"] == "vertical":
+            ui.add_element(
+                "display-password",
+                LabeledValue(
+                    color=BLACK,
+                    label="",
+                    value="",
+                    position=v_pos,
+                    label_font=fonts.Bold,
+                    text_font=fonts.Small,
+                ),
+            )
         else:
             # default to horizontal
-            ui.add_element('display-password', LabeledValue(color=BLACK, label='', value='',
-                                                   position=h_pos,
-                                                   label_font=fonts.Bold, text_font=fonts.Small))
+            ui.add_element(
+                "display-password",
+                LabeledValue(
+                    color=BLACK,
+                    label="",
+                    value="",
+                    position=h_pos,
+                    label_font=fonts.Bold,
+                    text_font=fonts.Small,
+                ),
+            )
 
     def on_unload(self, ui):
         with ui._lock:
-            ui.remove_element('display-password')
+            ui.remove_element("display-password")
 
     def on_ui_update(self, ui):
         if self.peers_detected:
-            ui.hide_element('display-password')
+            ui.hide_element("display-password")
         else:
-            last_line = 'tail -n 1 /root/handshakes/wpa-sec.cracked.potfile | awk -F: \'{print $3 " - " $4}\''
-            ui.set('display-password',
-                    "%s" % (os.popen(last_line).read().rstrip()))
-            ui.show_element('display-password')
-
-        
+            last_line = "tail -n 1 /root/handshakes/wpa-sec.cracked.potfile | awk -F: '{print $3 \" - \" $4}'"
+            ui.set("display-password", "%s" % (os.popen(last_line).read().rstrip()))
+            ui.show_element("display-password")
 
     # Called when a new peer is detected
     def on_peer_detected(self, agent, peer):
